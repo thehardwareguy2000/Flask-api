7d04f5bebce30b6e0656504482732f34493808c0
thehardwareguy2000 <54811767+thehardwareguy2000@users.noreply.github.com>
2019-10-10 10:42:42 +0530

    Update app.py
    made few changes for file to parse

--- deploying-ml-model-using-flask-master/app.py
+++ deploying-ml-model-using-flask-master/app.py
@@ -1,50 +1,120 @@
-from flask import Flask, render_template,url_for,request
-import pandas as pd
+from flask import Flask, render_template,url_for,request,send_file
 import pickle
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.externals import joblib
+from imutils.perspective import four_point_transform
+from imutils import contours
+import numpy as np
+import imutils
+import cv2
+import xlsxwriter
+import flask_excel as excel
+import pandas as pd
+
+
+
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/predict', methods = ['POST'])
 def predict():
-	df = pd.read_csv("data/Youtube01-Psy.csv")
-	df_data = df[['CONTENT', 'CLASS']]
-	# Features and Labels
-	df_x = df_data['CONTENT']
-	df_y = df_data.CLASS
-	#Extract the features with countVectorizer
-	corpus = df_x
-	cv = CountVectorizer()
-	X = cv.fit_transform(corpus)
-	from sklearn.model_selection import train_test_split
+    image = cv2.imread("test.jpg")
+    print(image.shape)
 
-	X_train, X_test, y_train, y_test = train_test_split(X, df_y, test_size = 0.33, random_state = 42)
+ 
+    workbook = xlsxwriter.Workbook('Attendence.xlsx')
+    worksheet = workbook.add_worksheet("sheet")
 
-	#Navie Bayes
-	clf = MultinomialNB()
-	clf.fit(X_train, y_train)
-	clf.score(X_test, y_test)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(blurred, 75, 200)
 
-	#Save Model
-	joblib.dump(clf, 'model.pkl')
-	print("Model dumped!")
 
-	#ytb_model = open('spam_model.pkl', 'rb')
-	clf = joblib.load('model.pkl')
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    docCnt = None
 
-	if request.method == 'POST':
-		comment = request.form['comment']
-		data = [comment]
-		vect = cv.transform(data).toarray()
-		my_prediction = clf.predict(vect)
+    if len(cnts) > 0:
 
-	return render_template('result.html', prediction = my_prediction)
+            cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+            for c in cnts:
+                peri = cv2.arcLength(c, True)
+                approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+                if len(approx) == 4:
+                    docCnt = approx
+                    break
+            else :
+                print("get the picture of full image")
+    
+    
+
+    paper = four_point_transform(image, docCnt.reshape(4, 2))
+    warped = four_point_transform(gray, docCnt.reshape(4, 2))
+#otsu threshold algo
+    thresh = cv2.threshold(warped, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    questionCnts = []
+
+
+    for c in cnts:
+	
+        (x, y, w, h) = cv2.boundingRect(c)
+        ar = w / float(h)
+
+	
+        if w >= 10 and h >= 20 and ar >= 0.9 and ar <= 1.1:
+            questionCnts.append(c)
+
+
+    questionCnts = contours.sort_contours(questionCnts,method="top-to-bottom")[0]
+    correct = 0
+    roll=8312
+#col = 0
+    row = 0
+    for (q, i) in enumerate(np.arange(0, len(questionCnts), 5)):
+
+        cnts = contours.sort_contours(questionCnts[i:i + 5])[0]
+        bubbled = None
+        g =0
+        col = 0
+        z=1
+        for (j, c) in enumerate(cnts):
+            mask = np.zeros(thresh.shape, dtype="uint8")
+            cv2.drawContours(mask, [c], -1, 255, -1)
+    
+            mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+            total = cv2.countNonZero(mask)
+            
+            bubbled = (total, j)
+            if bubbled > (1000,0):
+                g = g + 1
+                print(z)
+                
+            z=z+1
+        score = (g / 5.0) * 100
+        print(roll,":-",score)
+        print("-----------------------------------------")
+        worksheet.write(row, col, roll) 
+        worksheet.write(row, col + 1, score) 
+        row += 1
+        roll=roll+1
+	
+        
+    workbook.close()
+    #if request.method == 'POST':
+       # comment = request.form['comment']
+        #data = [comment]
+        #vect = cv.transform(data).toarray()
+        #my_prediction = clf.predict(vect)   , prediction = my_prediction
+        
+    return render_template('result.html')
+    return send_file( workbook , attachment_filename="Attendence.xlsx", as_attachment=True)
 
 
 if __name__ == '__main__':
